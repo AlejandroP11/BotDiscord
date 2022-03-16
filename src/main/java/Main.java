f81a8247a2850a6ea3102adb972139068e906872
Jose Alejandro Pereiro Gabian <jpereirogabian@danielcastelao.org>
2022-03-16 13:58:34 +0100

    token introducido

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,7 @@ import discord4j.core.object.entity.channel.MessageChannel;
 
 public class Main {
     public static void main(String[] args) {
-        final String token = args[0]; //token generado desde el developer portal de discord creando una nueva aplicacion y generando un nuevo bot
+        final String token = "OTUzNjMwNTIxOTk1NDM2MTE0.YjHXog.8cxlaSru6-slzKiHi8ld_RDV2PE"; //token generado desde el developer portal de discord creando una nueva aplicacion y generando un nuevo bot
         final DiscordClient client = DiscordClient.create(token);
         final GatewayDiscordClient gateway = client.login().block();
 
