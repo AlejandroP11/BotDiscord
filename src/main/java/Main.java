4dfb90f7447186b678e27d481a36bc65bd027116
Jose Alejandro Pereiro Gabian <jpereirogabian@danielcastelao.org>
2022-04-22 10:06:17 +0200

    Coneccion bot drive

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,17 @@
 
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.DriveScopes;
+import com.google.api.services.drive.model.FileList;
 import discord4j.core.DiscordClient;
 import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.message.MessageCreateEvent;
@@ -7,10 +20,54 @@ import discord4j.core.object.entity.channel.MessageChannel;
 import discord4j.core.spec.EmbedCreateSpec;
 import discord4j.rest.util.Color;
 
-import java.io.File;
+import java.io.*;
+import java.security.GeneralSecurityException;
 import java.time.Instant;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
+
+    /** Application name. */
+    private static final String APPLICATION_NAME = "Google Drive API Java Quickstart";
+    /** Global instance of the JSON factory. */
+    private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
+    /** Directory to store authorization tokens for this application. */
+    private static final String TOKENS_DIRECTORY_PATH = "tokens";
+
+    /**
+     * Global instance of the scopes required by this quickstart.
+     * If modifying these scopes, delete your previously saved tokens/ folder.
+     */
+    private static final List<String> SCOPES = Collections.singletonList(DriveScopes.DRIVE_METADATA_READONLY);
+    private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
+
+    /**
+     * Creates an authorized Credential object.
+     * @param HTTP_TRANSPORT The network HTTP Transport.
+     * @return An authorized Credential object.
+     * @throws IOException If the credentials.json file cannot be found.
+     */
+    private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
+        // Load client secrets.
+        InputStream in = Main.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
+        if (in == null) {
+            throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
+        }
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+        // Build flow and trigger user authorization request.
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
+                .setAccessType("offline")
+                .build();
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+        Credential credential = new AuthorizationCodeInstalledApp(flow, receiver).authorize("964719180311-46p8bmj564j907ce3069dnd79pus9nl5.apps.googleusercontent.com");
+        //returns an authorized Credential object.
+        return credential;
+    }
+
     public static void main(String[] args) {
         final String token = args[0]; //token generado desde el developer portal de discord creando una nueva aplicacion y generando un nuevo bot
         //el token se pasa por la configuracion de ejecucion
@@ -34,12 +91,14 @@ public class Main {
             //si el mensaje recibido es !ping
             if ("!ping".equals(message.getContent())) {
                 final MessageChannel channel = message.getChannel().block();
+                //devolver un !Pong
                 channel.createMessage("Pong!").block();
             }
 
             //si el mensaje recibido es un saludito
             if ("Un saludito".equalsIgnoreCase(message.getContent())){
                 final MessageChannel channel = message.getChannel().block();
+                //devolver el emberd creado anteriormente
                 channel.createMessage(embed).block();
             }
 
@@ -49,7 +108,35 @@ public class Main {
                 File files[] = file.listFiles();
                 for (int i = 0; i < files.length; i++) {
                     final MessageChannel channel = message.getChannel().block();
+                    //devolver los nombres de los archivos
                     channel.createMessage(files[i].getName()).block();
+                }
+            }
+            //si el mensaje es !buscar
+            if("!buscar".equals(message.getContent())){
+                final MessageChannel channel = message.getChannel().block();
+                final NetHttpTransport HTTP_TRANSPORT;
+                try {
+                    HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+                    Drive service = new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+                            .setApplicationName(APPLICATION_NAME)
+                            .build();
+                    FileList result = service.files().list()
+                            .setQ("mimeType='image/jpeg' or mimeType='application/pdf'")
+                            .setFields("nextPageToken, files(id, name)")
+                            .execute();
+                    List<com.google.api.services.drive.model.File> files = result.getFiles();
+                    if (files == null || files.isEmpty()) {
+                        channel.createMessage("Archivos no encontrados");
+                    } else {
+                        for (com.google.api.services.drive.model.File file : files) {
+                            channel.createMessage(file.getName()).block();
+                        }
+                    }
+                } catch (GeneralSecurityException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
             }
         });
